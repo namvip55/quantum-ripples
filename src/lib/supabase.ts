449ab7a7aa908e2a6eca76877d62d696f6da7589
namnguyen <namcho376@gmail.com>
2026-04-18 16:12:19 +0700

    chore: setup environment variables for supabase to avoid exposing keys

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://kujabhqmjkxjsxncnltg.supabase.co";
-const supabaseKey = "sb_publishable_ZPS8wPxA5KzsrJhc0nB_6A_akZRjdRZ";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "";
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
